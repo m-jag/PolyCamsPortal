2e95a1533f4b7531ec0439c15e10ac2668dc9dfb
Matt Jag <jagodzinskimatt@gmail.com>
2018-11-16 05:02:34 -0500

    Converted Settings Activity to Fragment

--- app/src/main/java/edu/floridapoly/polycamsportal/MainActivity.java
+++ app/src/main/java/edu/floridapoly/polycamsportal/MainActivity.java
@@ -16,6 +16,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -30,6 +31,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
 
+        Intent intent = getIntent();
+        username = intent.getStringExtra("username");
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -37,13 +41,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        View headerView = navigationView.getHeaderView(0);
+        TextView navUsername = (TextView) headerView.findViewById(R.id.username_header);
+        navUsername.setText(username);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
             R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
-        Intent intent = getIntent();
-        username = intent.getStringExtra("username");
 
         FragmentManager fm = getSupportFragmentManager();
 
--- app/src/main/java/edu/floridapoly/polycamsportal/SettingsFragment.java
+++ app/src/main/java/edu/floridapoly/polycamsportal/SettingsFragment.java
@@ -4,16 +4,37 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import edu.floridapoly.polycamsportal.R;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SettingsFragment extends Fragment {
+    private static final String TAG = "SettingsFragment";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        Button color_settings_button = (Button) view.findViewById(R.id.color_settings_btn);
+        color_settings_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Color Settings", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button font_size_button = (Button) view.findViewById(R.id.font_size_btn);
+        font_size_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Font Size", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
